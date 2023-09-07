bb437ea847f238ca2d0a5413db24d27867bec529
JOPloume <juosth@gmail.com>
2023-09-07 14:51:08 +0000

    Remove dashboard stuff from weather pipeline

--- surveyweathertool/pipeline.py
+++ surveyweathertool/pipeline.py
@@ -1,4 +1,3 @@
-from src.weather.weather_pipeline import combine_map_weather
 from src.weather.source_weather_data import get_weather_data
 from src.weather.constants import (
     RAW_PRECIPITATION_PATH,
@@ -12,22 +11,6 @@ from src.weather.constants import (
 def run_pipeline(
     source_weather_data: bool == False,
 ):
-    weather_data_name = (
-        "temperature"  # we should create drop menu for this in the dashboard
-    )
-    target_epsg = 4326  # we should create drop menu for this in the dashboard
-    col_dissolve = "admin1Pcod"  # we should create drop menu for this in the dashboard
-    level = "season"  # we should create drop menu for this in the dashboard
-    if level == "daily":
-        agg_dict = {weather_data_name: "mean"}
-        level = weather_data_name
-    else:
-        agg_dict = {
-            "mean": "mean",
-            "min": "mean",
-            "max": "mean",
-        }
-    # get weather data
     (
         nigeria_shape_df,
         weather_temperature_df,
