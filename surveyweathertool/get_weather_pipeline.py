d32ef4eea43eb4300051ed3bdaf694724084c46d
JOPloume <juosth@gmail.com>
2023-09-07 17:47:49 +0000

    Update to local nigeria shapepath file

--- surveyweathertool/get_weather_pipeline.py
+++ surveyweathertool/get_weather_pipeline.py
@@ -2,7 +2,6 @@ from src.weather.source_weather_data import get_weather_data
 from src.weather.constants import (
     RAW_PRECIPITATION_PATH,
     RAW_TEMPERATURE_PATH,
-    NIGERIA_SHAPE_PATH_FILE,
     SPI_PATH_FILE,
     GEO_COLUMNS,
 )
@@ -18,7 +17,7 @@ def run_pipeline(
     ) = get_weather_data(
         spi_path=SPI_PATH_FILE,
         source_weather_data=source_weather_data,
-        nigeria_shape_path=NIGERIA_SHAPE_PATH_FILE,
+        nigeria_shape_path="data/nga_admin/nga_admbnda_adm2_osgof_20170222.shp",
         data_path_precipitation=RAW_PRECIPITATION_PATH,
         data_path_temperature=RAW_TEMPERATURE_PATH,
         GEO_COLUMNS=GEO_COLUMNS,
